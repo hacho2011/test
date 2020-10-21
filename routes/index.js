c01e0bde96e09bb780b03054edf0233bfa97bcdd
kwonhygge <kitteniso1103@gmail.com>
2020-10-21 21:25:45 +0900

    Adds div container for avoiding duplicated css properties for about page, edited footer's subscribe button to use global btn class

--- routes/index.js
+++ routes/index.js
@@ -2,37 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 
-router.get('/member', function(req, res, next) {
+router.get('/member', function (req, res, next) {
   res.render('introduction/member', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   res.render('introduction/about', { title: 'Express' });
 });
 
-router.get('/board', function(req, res, next) {
+router.get('/board', function (req, res, next) {
   res.render('board/board', { title: 'Express' });
 });
 
-router.get('/notice', function(req, res, next) {
+router.get('/notice', function (req, res, next) {
   res.render('notice/notice', { title: 'Express' });
 });
 
-router.get('/production', function(req, res, next) {
+router.get('/production', function (req, res, next) {
   res.render('production/production', { title: 'Express' });
 });
 
-router.get('/program', function(req, res, next) {
+router.get('/program', function (req, res, next) {
   res.render('program/program', { title: 'Express' });
 });
 
-router.get('/youtube', function(req, res, next) {
+router.get('/youtube', function (req, res, next) {
   res.render('youtube/youtube', { title: 'Express' });
+});
+router.get('/column', function (req, res, next) {
+  res.render('column/column', { title: 'Express' });
 });
 
 
